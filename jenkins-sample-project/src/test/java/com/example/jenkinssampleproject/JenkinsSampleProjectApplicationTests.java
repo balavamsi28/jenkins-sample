69b163ba4ce8b4d99297cc3b930150a766107a87
Bala vamsi <41420104+balavamsi28@users.noreply.github.com>
2024-05-09 12:16:06 +0530

    1st commit

---
+++ jenkins-sample-project/src/test/java/com/example/jenkinssampleproject/JenkinsSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinssampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
